1229ec68ecf309ca39f771b2f6f6a83156391a6a
drewhsu86 <bowserhsu@gmail.com>
2020-05-06 14:41:18 -0400

    all 3 types of calls return something on browser (all gets)

---
+++ server.js
@@ -0,0 +1,62 @@
+// idea based on:
+// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+// get request from my codewars app
+// becomes api request to codewars 
+const url = `https://www.codewars.com/api/v1/`
+
+// we have 3 types of api calls
+// looking up user account 
+// looking up what code challenges they finished
+// and looking up the code challenge id 
+
+// function for our axios call 
+function axiosCall(newURL, res) {
+  // change header to avoid CORS error 
+  res.header('Access-Control-Allow-Origin', '*')
+
+  axios.get(newURL, {
+    Accept: "application/json"
+  })
+    .then((response) => {
+      // console.log(response)
+      res.json({ data: response.data })
+    })
+    .catch((er) => {
+      // console.log(er)
+      res.send(er)
+    })
+}
+
+// looking up user 
+app.get('/users/:username', (req, res) => {
+  const newURL = url + 'users/' + req.params.username
+
+  axiosCall(newURL, res)
+})
+
+// looking up a user's code challenges
+// users/:username/code-challenges/completed
+
+app.get('/users/:username/code-challenges/completed', (req, res) => {
+  const newURL = url + 'users/' + req.params.username + '/code-challenges/completed'
+
+  axiosCall(newURL, res)
+})
+
+// looking up a code challenge by ID
+// code-challenges/id
+app.get('/code-challenges/:id', (req, res) => {
+  const newURL = url + 'code-challenges/' + req.params.id
+
+  axiosCall(newURL, res)
+})
+
+// app port and listen
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
